7c79d41d25ae4e6af718826e4521e2fefa6c5472
Jan Adamski <j.adamski@zsebydgoszcz.onmicrosoft.com>
2023-03-18 16:08:59 +0100

    added ToDoModel

---
+++ ToDoList/Models/ToDoModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ToDoList.Models
+{
+    public class ToDoModel
+    {
+        public ToDoModel()
+        {
+            
+        }
+
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime Day { get; set; }
+    }
+}

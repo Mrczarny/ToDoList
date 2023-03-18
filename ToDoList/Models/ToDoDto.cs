d9b78970d80f0f30240509a04cd668f7fc98d41e
Jan Adamski <j.adamski@zsebydgoszcz.onmicrosoft.com>
2023-03-18 18:48:48 +0100

    added ToDoDto

---
+++ ToDoList/Models/ToDoDto.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ToDoList.Models
+{
+    public class ToDoDto
+    {
+        public ToDoDto(string name, string description, DateTime date)
+        {
+            this.Guid = Guid.NewGuid();
+            this.Name = name;
+            this.Description = description;
+            this.Date = date;
+        }
+
+        public ToDoDto(ToDoModel m)
+        {
+            Date = m.Date;
+            Name = m.Name;
+            Description = m.Description;
+        }
+
+        public Guid Guid {
+            get => this.Guid;
+            set => Guid.NewGuid();
+        } 
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime Date { get; set; }
+    }
+}

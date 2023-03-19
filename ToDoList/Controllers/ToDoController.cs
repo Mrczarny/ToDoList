857fbca7bcfd347f356f464879220244ee46f49a
Jan Adamski <j.adamski@zsebydgoszcz.onmicrosoft.com>
2023-03-20 00:23:16 +0100

    GetWeekTodos and EditToDo added to ToDoController

--- ToDoList/Controllers/ToDoController.cs
+++ ToDoList/Controllers/ToDoController.cs
@@ -39,6 +39,56 @@ namespace ToDoList.Controllers
 
         }
 
+        [HttpGet("{date:datetime}")]
+        public ActionResult<IEnumerable<ToDoDto>> GetWeekToDos(DateTime date)
+        {
+            try
+            {
+                date = date.Subtract(TimeSpan.FromDays((int) date.DayOfWeek)).Date;
+                var todos = new List<ToDoDto>();
+                var monthsTodo = _context.ToDoSet.Where(x => x.Date.Year == date.Year && x.Date.Month == date.Month).AsEnumerable();
+                foreach (var todo in monthsTodo.Where(x => x.Date.Subtract(date) > TimeSpan.Zero && x.Date.Subtract(date) < TimeSpan.FromDays(7) ))
+                {
+                    var dto = new ToDoDto(todo);
+                    HttpContext.Session.Set(dto.Guid.ToString(), BitConverter.GetBytes(todo.Id));
+                    todos.Add(dto);
+                }
+                return todos;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        [HttpPut]
+        [HttpPost("Edit")]
+        public ActionResult EditToDo(ToDoDto model)
+        {
+            try
+            {
+                var entry = _context.Find<ToDoModel>(BitConverter.ToInt64(HttpContext.Session.Get(model.Guid.ToString())));
+                if (entry != null)
+                {
+                    entry.Date = model.Date;
+                    entry.Name = model.Name;
+                    entry.Description = model.Description;
+                    _context.ToDoSet.Update(entry);
+                    _context.SaveChanges();
+                }
+                return NotFound("ToDo not found");
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, e.Message);
+                return Problem();
+            }
+        }
+
+
+
+
         [HttpPost]
         public ActionResult AddToDo(ToDoDto model)
         {
